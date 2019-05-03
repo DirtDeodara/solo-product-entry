c31ed8a82e6ef3cd0d811854ab665278903a0886
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-02 20:57:47 -0700

    added name to query string in the row link

--- src/customers.js
+++ src/customers.js
@@ -7,10 +7,28 @@ const customers = customerApi.getAll();
 for(let i = 0; i < customers.length; i++) {
     const customer = customers[i];
 
-    const tr = document.createElement('tr');
 
+    // input: applicant
+
+    const tr = document.createElement('tr');
+    //we are creating the table cell    
     const nameCell = document.createElement('td');
-    nameCell.textContent = customer.name;
+    // creating the anchor link <a>
+    const link = document.createElement('a');
+    
+    //create a search params object
+    const searchParams = new URLSearchParams();
+    // set the name key with the customers name as the value
+    searchParams.set('name', customer.name);
+    //use the formatted search string as the end of the link href
+    //that goes to the detail page
+    link.href = 'order.html?' + searchParams.toString();
+
+    // also use the customer's name as the display text for the link
+    link.textContent = customer.name;
+    // add the link to the name cell
+    nameCell.appendChild(link)
+    // add the name cell to the new table row
     tr.appendChild(nameCell);
 
     const destinationCell = document.createElement('td');
@@ -28,6 +46,9 @@ for(let i = 0; i < customers.length; i++) {
     }
     addOnsCell.textContent = addOnsList;
     tr.appendChild(addOnsCell);
+    
+
+    //output: tr
 
     tbody.appendChild(tr);
 }
